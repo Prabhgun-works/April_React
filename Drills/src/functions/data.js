09d3f0070f93790abfe88a632ba314402cb32504
Prabhgun Singh <prabhgun.web@gmail.com>
2025-04-30 09:23:12 +0530

    Delete Drills directory

--- Drills/src/functions/data.js
+++
@@ -1,9 +0,0 @@
-// data.js
-
-export const users = [
-    { id: 1, name: "Prabhgun", age: 23, role: "Developer" },
-    { id: 2, name: "Kavya", age: 25, role: "Designer" },
-    { id: 3, name: "Sargun", age: 22, role: "Developer" },
-    { id: 4, name: "Vani", age: 27, role: "Manager" },
-    { id: 5, name: "Test User", age: 30, role: "Developer" },
-  ];
